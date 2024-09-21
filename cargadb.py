cd5c0d802f44db8e64e6cad297abeb983cbcd945
Felipe Celestino Laureano <164645975+LaureanoFelipe@users.noreply.github.com>
2024-09-21 12:49:17 +0000

    Ok

--- cargadb.py
+++ cargadb.py
@@ -24,10 +24,10 @@ def load_sql_file(filepath, db_params):
 
 # Parâmetros de conexão
 db_params = {
-    'host': 'dpg-crmpgj5umphs739ipld0-a.oregon-postgres.render.com',
-    'database': 'teste_qthi',
+      'host': 'dpg-crnba1g8fa8c738cov20-a.oregon-postgres.render.com',
+    'database': 'db_qcv8',
     'user': 'teste',
-    'password': 'YzxANlVWAXnecF1eARNWVuPd0gwVgOFb'
+    'password': 'OgX9bqYUwAvDE8eViKWbu142dykbTt8K'
 }
 
 
